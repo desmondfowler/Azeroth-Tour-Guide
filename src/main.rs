424c565910b7937924528598bc761f5923b1f668
desmondfowler <desmondfowler14@gmail.com>
2025-02-25 14:52:03 -0600

    Whoops, chose fullstack when I wanted bare-bones in dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use dioxus::prelude::*;
 
-const HERO_CSS: Asset = asset!("/assets/styling/hero.css");
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Hero {}
+
+    }
+}
 
 #[component]
 pub fn Hero() -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: HERO_CSS }
-
         div {
             id: "hero",
             img { src: HEADER_SVG, id: "header" }
