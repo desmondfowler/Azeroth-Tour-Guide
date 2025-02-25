3538f5af171cd9f3f0df1201f31bedf82909e5f5
desmondfowler <desmondfowler14@gmail.com>
2025-02-25 14:35:41 -0600

    Refactor project structure: remove frontend and backend packages, recreate with dx init

--- ui/src/hero.rs
+++ ui/src/hero.rs
@@ -1,26 +1,13 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HERO_CSS: Asset = asset!("/assets/styling/hero.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
-
-fn main() {
-    dioxus::launch(App);
-}
-
-#[component]
-fn App() -> Element {
-    rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
-
-    }
-}
 
 #[component]
 pub fn Hero() -> Element {
     rsx! {
+        document::Link { rel: "stylesheet", href: HERO_CSS }
+
         div {
             id: "hero",
             img { src: HEADER_SVG, id: "header" }
