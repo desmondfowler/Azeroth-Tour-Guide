b69b2b0052ec3474cfcf3e993617587b213d451f
desmondfowler <desmondfowler14@gmail.com>
2025-03-11 12:54:01 -0500

    Update image source in Azeroth component from JPG to PNG

--- frontend/src/components/Azeroth.tsx
+++ frontend/src/components/Azeroth.tsx
@@ -6,7 +6,7 @@ export default function Azeroth() {
     <main className="flex-grow py-2">
       <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 ">
         <img 
-          src="azeroth.jpg" 
+          src="azeroth.png" 
           className="mt-2 w-full max-w-full mx-auto rounded-lg shadow-md" 
           alt="A paper-art style map of Azeroth" 
         />
