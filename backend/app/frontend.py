c6a7768118ac7dbe65c349a45dc3e32342844618
desmondfowler <desmondfowler14@gmail.com>
2025-03-12 18:22:17 -0500

    Fix routing so it allows react router to do its thing

--- backend/app/frontend.py
+++ backend/app/frontend.py
@@ -1,14 +1,22 @@
+import os
+from pathlib import Path
 from flask import send_from_directory
 
 def register_frontend_routes(app):
-    """Registers frontend-serving routes with the Flask app."""
-    @app.route('/')
-    @app.route('/azeroth')
-    @app.route('/contact')
-    @app.route('/region/<path:subpath>')
-    def serve_index(subpath=None):
-        return send_from_directory(app.static_folder, 'index.html')
-    
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    FRONTEND_DIR = BASE_DIR / ".." / "frontend" / "dist"
+    app.logger.debug(f"FRONTEND_DIR set to: {FRONTEND_DIR}")
+
+    @app.route('/', defaults={'path': ''})
     @app.route('/<path:path>')
-    def static_files(path):
-        return send_from_directory(app.static_folder, path)
+    def serve_frontend(path):
+        app.logger.debug(f"Serving frontend for path: '{path}'")
+        full_path = os.path.join(FRONTEND_DIR, path)
+        app.logger.debug(f"Checking if path exists: {full_path}")
+        if path != "" and os.path.exists(full_path):
+            app.logger.debug(f"Serving static file: {path}")
+            return send_from_directory(FRONTEND_DIR, path)
+        app.logger.debug("Serving index.html")
+        index_path = os.path.join(FRONTEND_DIR, 'index.html')
+        app.logger.debug(f"Index.html path: {index_path}")
+        return send_from_directory(FRONTEND_DIR, 'index.html')
