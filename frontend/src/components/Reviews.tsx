45aa6b163c7c9808d4f57568571e35b9f20129a8
desmondfowler <desmondfowler14@gmail.com>
2025-03-12 21:06:54 -0500

    Refactor Reviews component to fetch random reviews from API and display them

--- frontend/src/components/Reviews.tsx
+++ frontend/src/components/Reviews.tsx
@@ -1,19 +1,18 @@
 // components/Reviews.tsx
 
-export default function Reviews({ count }: { count: number }) {
-  const numReviews = Math.max(3, count); // Math.min(count, 10);
+import { useEffect, useState } from "react";
 
+export default function Reviews() {
+  const [reviews, setReviews] = useState<{ text: string; author: string }[]>(
+    []
+  );
 
-  function getRandomReviews(reviewsArray: typeof reviews, num: number) {
-    const shuffled = [...reviewsArray];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled.slice(0, num);
-  }
-
-  const randomReviews = getRandomReviews(reviews, numReviews);
+  useEffect(() => {
+    fetch("/api/random-reviews")
+      .then((response) => response.json())
+      .then((data) => setReviews(data.reviews))
+      .catch((error) => console.error("Error fetching reviews: ", error));
+  });
 
   return (
     <div className="bg-gray-900 py-6">
@@ -21,7 +20,7 @@ export default function Reviews({ count }: { count: number }) {
         Read our reviews!
       </h3>
       <div className="max-w-4xl mx-auto space-y-6 py-6">
-        {randomReviews.map((review, index) => (
+        {reviews.map((review, index) => (
           <div
             key={index}
             className="bg-gray-800 p-6 rounded-xl shadow-lg text-gray-300 text-center border border-indigo-600"
