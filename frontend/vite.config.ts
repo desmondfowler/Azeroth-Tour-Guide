03836a3406ff4f0a33a208f7eefe3ef23131047c
desmondfowler <desmondfowler14@gmail.com>
2025-03-18 14:33:12 -0500

    Simplify TS config and add testing support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest/config" />
+
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -9,5 +11,10 @@ export default defineConfig({
   base: "/",
   build: {
     outDir: "dist",
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+    globals: true,
   },
 });
